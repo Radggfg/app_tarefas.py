f58208a604dc15d2cdc0b4eb4141b29b5aac753f
RAFAEL DE SOUZA MAROSTICA <134351362+Radggfg@users.noreply.github.com>
2024-07-10 18:29:12 -0300

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+icon_editar = carregar_imagem(caminho_editar)
+# icon_editar = icon_editar.subsample(3, 3)
+
+# icon_deletar = carregar_imagem(caminho_deletar)
+# icon_deletar = icon_deletar.subsample(3, 3)
